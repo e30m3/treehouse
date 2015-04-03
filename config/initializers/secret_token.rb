38ccccb35ace937cea313205df7ab2b0d1bb0035
Beau Christensen <beau.christensen@gmail.com>
2015-04-03 15:20:03 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Treehouse::Application.config.secret_key_base = '1894abe34e02505cfa37a08cba051e4e847007f6a2ed645d4a3b64e94de69fb2144285e0efeb7886dbbc9cebac9d4651ac0d13b44b5ccca8e3ffe6cc56b7760c'
